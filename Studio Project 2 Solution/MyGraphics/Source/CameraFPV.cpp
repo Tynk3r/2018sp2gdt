d0aa809fb805935baa1085e0ff06bf32ce46aa83
Mohammad Rafiq <googaabumtum@gmail>
2018-02-13 18:30:10 +0800

    lil fix

--- Studio Project 2 Solution/MyGraphics/Source/CameraFPV.cpp
+++ Studio Project 2 Solution/MyGraphics/Source/CameraFPV.cpp
@@ -33,12 +33,7 @@ void CameraFPV::Init(const Vector3& pos, const Vector3& target, const Vector3& u
 
 void CameraFPV::Update(double dt)
 {
-	
-	//Temp Var
-	bool PrintInfo = true;
-
-	if (SceneManager::instance()->GetSceneID() == SceneManager::SCENEID_MAIN)
-	{
+	if (SceneManager::instance()->GetSceneID() == SceneManager::SCENEID_MAIN) {
 		static const float CAMERA_SPEED = 4.0f;
 		static float MOVEMENT_SPEED = 30.0f;
 
@@ -101,19 +96,11 @@ void CameraFPV::Update(double dt)
 		{
 			position += up * (float)(MOVEMENT_SPEED * dt);
 			target += up * (float)(MOVEMENT_SPEED * dt);
-			if (SceneManager::instance()->SC_SceneList[SceneManager::instance()->GetSceneID()]->collision(position, target)) {
-				position -= up * (float)(MOVEMENT_SPEED * dt);
-				target -= up * (float)(MOVEMENT_SPEED * dt);
-			}
 		}
 		if (Application::IsKeyPressed(17)) // -y
 		{
 			position -= up * (float)(MOVEMENT_SPEED * dt);
 			target -= up * (float)(MOVEMENT_SPEED * dt);
-			if (SceneManager::instance()->SC_SceneList[SceneManager::instance()->GetSceneID()]->collision(position, target)) {
-				position += up * (float)(MOVEMENT_SPEED * dt);
-				target += up * (float)(MOVEMENT_SPEED * dt);
-			}
 		}
 		if (Application::IsKeyPressed(VK_LSHIFT))
 		{
@@ -127,28 +114,43 @@ void CameraFPV::Update(double dt)
 		{
 			Reset();
 		}
-		
 		prevX = Application::GetCursorX();
 		prevY = Application::GetCursorY();
 	}
-	else if(SceneManager::instance()->GetSceneID() == SceneManager::SCENEID_1)
-	{
+	else if (SceneManager::instance()->GetSceneID() == SceneManager::SCENEID_1) {
+		//Set Camera Speed for Scene1
 		static const float CAMERA_SPEED = 1.0f;
 		static float MOVEMENT_SPEED = 20.0f;
 
-		double yaw = -horizMove*dt;
+		//Set Camera Variables
 		Vector3 view = (target - position).Normalized();
 		Vector3 right = view.Cross(up);
 		right.y = 0;
 		right.Normalize();
 		up = right.Cross(view).Normalized();
+		Vector3 front = right.Cross(up);
+		front.Normalize();
+
 		Mtx44 rotation;
+
+		//Yawing
+		double yaw = -horizMove*dt;
 		rotation.SetToRotation(yaw, up.x, up.y, up.z);
 		view = rotation*view;
 		target = position + view;
+
+		//Pitching
 		double pitch = (vertMove*dt) / 25.0f;
 		target = target + Vector3(-up.x*pitch, -up.y*pitch, -up.z*pitch);
 
+		//Rolling
+		double roll = rollMove*dt;
+		rotation.SetToRotation(roll, front.x, front.y, front.z);
+		//TODO : ADD CODE TO ROLL CAMERA//
+		//
+		//
+
+		//Auto Movement in Direction
 		Vector3 view1(view.x, view.y, view.z);
 		position += view1 * (float)(MOVEMENT_SPEED * dt);
 		target += view1 * (float)(MOVEMENT_SPEED * dt);
@@ -157,6 +159,20 @@ void CameraFPV::Update(double dt)
 			target -= view1 * (float)(MOVEMENT_SPEED * dt);
 		}
 
+		if (Application::IsKeyPressed('A'))
+		{
+			if (rollMove != 20)
+				rollMove += (double)(2.0*CAMERA_SPEED);
+			else
+				rollMove = 20;
+		}
+		if (Application::IsKeyPressed('D'))
+		{
+			if (rollMove != -20)
+				rollMove -= (double)(2.0*CAMERA_SPEED);
+			else
+				rollMove = -20;
+		}
 		if (Application::IsKeyPressed(VK_UP))
 		{
 			if (up.y > 0)
@@ -214,22 +230,8 @@ void CameraFPV::Update(double dt)
 		{
 			Reset();
 		}
-		if (Application::IsKeyPressed('I'))
-		{
-			PrintInfo = false;
-		}
-
-
-		//Print out camera info in console
-		if (PrintInfo)
-		{
-			std::cout << "Position : " << position << std::endl;
-			std::cout << "Target : " << target << std::endl;
-			std::cout << "Up : " << up << std::endl;
-			std::cout << "View : " << view << std::endl;
-		}
 	}
-	else{
+	else {
 		static const float CAMERA_SPEED = 4.0f;
 		static float MOVEMENT_SPEED = 30.0f;
 
@@ -310,22 +312,20 @@ void CameraFPV::Update(double dt)
 		{
 			MOVEMENT_SPEED = 60.0f;
 		}
-		if (Application::IsKeyPressed('R'))
-		{
-			Reset();
-		}
 		else
 		{
 			MOVEMENT_SPEED = 30.0f;
 		}
-
+		if (Application::IsKeyPressed('R'))
+		{
+			Reset();
+		}
 		prevX = Application::GetCursorX();
 		prevY = Application::GetCursorY();
 	}
 }
 
-void CameraFPV::Reset()
-{
+void CameraFPV::Reset(){
 	position = defaultPosition;
 	target = defaultTarget;
 	up = defaultUp;
