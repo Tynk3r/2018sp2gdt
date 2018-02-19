bf2a932e762916e223410d833fc4b5fa19203685
Ang Zinc-I <angzinc@gmail.com>
2018-02-19 17:01:27 +0800

    Improved Models and Lighting for Scene1 Rings Created MyPtero class for Scene1 and 2 usage Added stand-by resources for future integration and implementation of MyPtero for Scene1

---
+++ Studio Project 2 Solution/MyGraphics/Source/MyPtero.cpp
@@ -0,0 +1,61 @@
+#include "MyPtero.h"
+
+MyPtero::MyPtero()
+{
+	hungry = true;
+	incubating = false;
+	pteroStage = P_EGG;
+	pteroDirection = 180.0f;
+	pteroMovementSpeed = 0.0f;
+	pteroSize = 10;
+
+	this->setPos(Vector3(0,0,0));
+	this->setBox(this->getPos(), pteroSize);
+
+	//Actual position setting expected in Scene
+	//Collision expected to be updated in Scene
+}
+
+MyPtero::MyPtero(float spd, float steer, float stab, float stam, float size)
+{
+	hungry = true;
+	incubating = false;
+	pteroStage = P_EGG;
+	pteroDirection = 180.0f;
+	pteroMovementSpeed = 0.0f;
+	pteroSize = size;
+
+	FlightSpeed = spd;
+	FlightSteerSpeed = steer;
+	FlightStability = stab;
+	FlightStamina = stam;
+
+	this->setPos(Vector3(0, 0, 0));
+	this->setBox(this->getPos(), pteroSize);
+
+	//Actual position setting expected in Scene
+	//Collision expected to be updated in Scene
+}
+
+
+MyPtero::~MyPtero()
+{
+}
+
+// Getters for Scene1 pointer;
+float MyPtero::getSpeed(void)
+{
+	return this->FlightSpeed;
+}
+float MyPtero::getSteerSpeed(void)
+{
+	return this->FlightSteerSpeed;
+}
+float MyPtero::getStability(void)
+{
+	return this->FlightStability;
+}
+float MyPtero::getStamina(void)
+{
+	return this->FlightStamina;
+}
