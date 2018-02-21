483f3ce23e31222002119ad82d285d8f47e8683d
NYPSIT\n506 <javierng2knyp@gmail.com>
2018-02-21 19:35:27 +0800

    Added supply demand checking for pricing and display for prices

--- Studio Project 2 Solution/MyGraphics/Source/Shop.cpp
+++ Studio Project 2 Solution/MyGraphics/Source/Shop.cpp
@@ -23,17 +23,57 @@ SHOP::SHOP()
 	coord[SHOP_INCREASE_AMOUNT_100	].setBox(Vector3(900, 195, 0), 120, 60, 1);
 	coord[SHOP_DECREASE_AMOUNT_100	].setBox(Vector3(900, 603, 0), 120, 60, 1);
 
-	cost[SHOP_BUY_REDFRUIT			] = 2;
+	cost[SHOP_BUY_REDFRUIT			] = 200;
 	cost[SHOP_BUY_BLUFRUIT			] = 2;
 	cost[SHOP_BUY_MEAT				] = 2;
 	cost[SHOP_BUY_TRAP				] = 2;
 	cost[SHOP_BUY_INCUBATOR			] = 2;
 
-	cost[SHOP_SELL_REDFRUIT			] = 1;
-	cost[SHOP_SELL_BLUFRUIT			] = 1;
-	cost[SHOP_SELL_MEAT				] = 1;
-	cost[SHOP_SELL_TRAP				] = 1;
-	cost[SHOP_SELL_INCUBATOR		] = 1;
+	cost[SHOP_SELL_REDFRUIT			] = (cost[SHOP_BUY_REDFRUIT	] / 2);
+	cost[SHOP_SELL_BLUFRUIT			] = (cost[SHOP_BUY_BLUFRUIT	] / 2);
+	cost[SHOP_SELL_MEAT				] = (cost[SHOP_BUY_MEAT		] / 2);
+	cost[SHOP_SELL_TRAP				] = (cost[SHOP_BUY_TRAP		] / 2);
+	cost[SHOP_SELL_INCUBATOR		] = (cost[SHOP_BUY_INCUBATOR] / 2);
+
+	supply[SHOP_BUY_REDFRUIT		] = 45;
+	supply[SHOP_BUY_BLUFRUIT		] = 45;
+	supply[SHOP_BUY_MEAT			] = 45;
+	supply[SHOP_BUY_TRAP			] = 45;
+	supply[SHOP_BUY_INCUBATOR		] = 45;
+}
+
+void SHOP::Update()
+{
+	for (int i = (SHOP_BUY_START + 1); i <= SHOP_BUY_END; i++)
+	{
+		if (supply[i] >= 45)
+		{
+			supply[i] = 45;
+		}
+		else if (supply[i] <= 0)
+		{
+			supply[i] = 1;
+		}
+
+		// Supply - Demand equation
+		cost[i] = ((999 / (supply[i] / 2)) - ((supply[i] - 1) / (supply[i] / 40)));
+
+		// Fail safes such that there will always be a price
+		if (cost[i] >= 999)
+		{
+			cost[i] = 999;
+		}
+		else if (cost[i] < 0)
+		{
+			cost[i] = 1;
+		}
+	}
+
+	cost[SHOP_SELL_REDFRUIT	] = (cost[SHOP_BUY_REDFRUIT	] / 2);
+	cost[SHOP_SELL_BLUFRUIT	] = (cost[SHOP_BUY_BLUFRUIT	] / 2);
+	cost[SHOP_SELL_MEAT		] = (cost[SHOP_BUY_MEAT		] / 2);
+	cost[SHOP_SELL_TRAP		] = (cost[SHOP_BUY_TRAP		] / 2);
+	cost[SHOP_SELL_INCUBATOR] = (cost[SHOP_BUY_INCUBATOR] / 2);
 }
 
 Vector3 SHOP::GetCoord(int i)
